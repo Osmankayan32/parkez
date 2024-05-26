b4672867ca98ef592b50972b0613172fc8213477
slymnyg <suleymanyag399@gmail.com>
2024-05-26 15:51:55 +0300

    uzaklık bilgisinin hesabı

--- lib/views/park_screen/park_screen.dart
+++ lib/views/park_screen/park_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -5,16 +6,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' as map;
+import 'package:latlong2/latlong.dart';
 import 'package:login_screen/models/otopark_model.dart';
 import 'package:login_screen/themes/light_theme.dart';
 import 'package:login_screen/views/map_screen/map_screen.dart';
 import 'package:login_screen/widgets/custom_bottomsheet.dart';
 import 'package:permission_handler/permission_handler.dart';
-
 import '../../widgets/wating_widgets.dart';
+import 'package:http/http.dart' as http;
 import '../kullanici_park_detay/kullanici_park_detay.dart';
 import 'controller/park_screen_controller.dart';
+
+class ListModel {
+  OtoparkModel otoparkModel;
+  String uzaklik;
+
+  ListModel({required this.otoparkModel, required this.uzaklik});
+}
 
 class ParkScreen extends StatefulWidget {
   final String plaka;
@@ -27,7 +36,91 @@ class ParkScreen extends StatefulWidget {
 
 class _ParkScreenState extends State<ParkScreen> {
   List<OtoparkModel> otoparklar = [];
+  /*
 
+  Future<List<ListModel>> calculat({required List<OtoparkModel> otoparklar}) async {
+    // kullanıcının konumunu al
+
+    LatLng currentPosition;
+    var status = await Permission.location.request();
+    if (status.isGranted) {
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      currentPosition = LatLng(position.latitude, position.longitude);
+    } else {
+      throw Exception("Konum izni verilmedi");
+    }
+
+    // Mesafe hesaplamak için yardımcı fonksiyon
+    double calculateDistance(LatLng start, LatLng end) {
+      final Distance distance = Distance();
+      return distance.as(LengthUnit.Kilometer, start, end);
+    }
+
+    // Otoparkların uzaklıklarını hesapla ve ListModel nesneleri oluştur
+    List<ListModel> listModels = otoparklar.map((e) {
+      String? konum = e.otoparkAdresi;
+      double? lat = double.parse(konum!.split(",")[0]);
+      double? long = double.parse(konum.split(",")[1]);
+      LatLng otoparkPosition = LatLng(lat, long);
+      double uzaklik = calculateDistance(currentPosition, otoparkPosition);
+      return ListModel(otoparkModel: e, uzaklik: uzaklik.toStringAsFixed(2));
+    }).toList();
+
+    // Listeyi yakından uzağa sıralama
+    listModels.sort((a, b) => double.parse(a.uzaklik).compareTo(double.parse(b.uzaklik)));
+
+    return listModels;
+  }
+
+   */
+
+  Future<String> calculateDrivingDistance(LatLng start, LatLng end) async {
+    const String apiKey = 'AIzaSyAl5TtsfNcQL0iLuG_STqqYcW5zgEV19no';
+    final String url = 'https://maps.googleapis.com/maps/api/directions/json?origin=${start.latitude},${start.longitude}&destination=${end.latitude},${end.longitude}&key=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      if (data['routes'].isNotEmpty) {
+        final route = data['routes'][0];
+        final legs = route['legs'][0];
+        final distance = legs['distance']['text'];
+        return distance;
+      } else {
+        throw Exception('No routes found');
+      }
+    } else {
+      throw Exception('Failed to load directions');
+    }
+  }
+
+  Future<List<ListModel>> calculat({required List<OtoparkModel> otoparklar}) async {
+    // Kullanıcının konumunu al
+    LatLng currentPosition;
+    var status = await Permission.location.request();
+    if (status.isGranted) {
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      currentPosition = LatLng(position.latitude, position.longitude);
+    } else {
+      throw Exception("Konum izni verilmedi");
+    }
+
+    // Otoparkların uzaklıklarını hesapla ve ListModel nesneleri oluştur
+    List<ListModel> listModels = [];
+    for (var otopark in otoparklar) {
+      String? konum = otopark.otoparkAdresi;
+      double? lat = double.parse(konum!.split(",")[0]);
+      double? long = double.parse(konum.split(",")[1]);
+      LatLng otoparkPosition = LatLng(lat, long);
+      String uzaklik = await calculateDrivingDistance(currentPosition, otoparkPosition);
+      listModels.add(ListModel(otoparkModel: otopark, uzaklik: uzaklik));
+    }
+
+    // Listeyi yakından uzağa sıralama
+    listModels.sort((a, b) => double.parse(a.uzaklik.split(' ')[0]).compareTo(double.parse(b.uzaklik.split(' ')[0])));
+
+    return listModels;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,15 +136,15 @@ class _ParkScreenState extends State<ParkScreen> {
                 return;
               }
 
-              Set<Marker> markers = otoparklar.map((e) {
+              Set<map.Marker> markers = otoparklar.map((e) {
                 String? konum = e.otoparkAdresi;
                 double? lat = double.parse(konum!.split(",")[0]);
                 double? long = double.parse(konum.split(",")[1]);
 
-                return Marker(
-                    markerId: MarkerId(e.firebaseId!),
-                    position: LatLng(lat, long),
-                    infoWindow: InfoWindow(title: e.otoparkIsmi!),
+                return map.Marker(
+                    markerId: map.MarkerId(e.firebaseId!),
+                    position: map.LatLng(lat, long),
+                    infoWindow: map.InfoWindow(title: e.otoparkIsmi!),
                     onTap: () {
                       showDialog(
                           context: context,
@@ -112,23 +205,38 @@ class _ParkScreenState extends State<ParkScreen> {
                   model.firebaseId = e.id;
                   return model;
                 }).toList();
+
                 otoparklar = data;
-                return ListView.builder(
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(
-                        data[index].otoparkIsmi!,
-                        style: TextStyle(
-                          fontSize: 19,
-                          fontWeight: FontWeight.bold,
-                          color: Themes.primaryColor,
-                        ),
-                      ),
-                      onTap: () => controller.otparkDetayaGit(context, data[index], widget.plaka),
-                    );
-                  },
-                  itemCount: data.length,
-                );
+                return FutureBuilder(
+                    future: calculat(otoparklar: data),
+                    builder: (context, AsyncSnapshot<List<ListModel>> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const WatingWidget();
+                      }
+
+                      if (snapshot.hasError) {
+                        return const Center(child: Text("Hata oluştu"));
+                      }
+
+                      List<ListModel> data = snapshot.data!;
+                      return ListView.builder(
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(
+                              data[index].otoparkModel.otoparkIsmi!,
+                              style: TextStyle(
+                                fontSize: 19,
+                                fontWeight: FontWeight.bold,
+                                color: Themes.primaryColor,
+                              ),
+                            ),
+                            subtitle: Text("Uzaklık: ${data[index].uzaklik}", style: const TextStyle(fontSize: 12, color: Colors.black)),
+                            onTap: () => controller.otparkDetayaGit(context, data[index].otoparkModel, widget.plaka),
+                          );
+                        },
+                        itemCount: data.length,
+                      );
+                    });
               });
         }));
   }
